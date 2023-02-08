906e62eed9a18d0eaa2f2201f7b78df30a96b263
Vikash Kushwaha <Vikash75.jm@gmail.com>
2023-02-08 22:50:37 +0530

    code line add

---
+++ Js/js-program.js
@@ -0,0 +1,56 @@
+// Alert command
+// alert("hello JavaScript");
+
+// Network requst our background runnig.
+console.log('Namaste Love Babber sir');
+
+// Fix value.
+     // const number = 12;
+     // number = 13;
+     // console.log(number + " fix value");
+
+// Variables.
+
+// Integer int
+let Number_1 = 50;
+let Number_2 = 40;
+let count = Number_1 + Number_2;
+console.log(count + " All Count");
+
+// float 
+
+let item_0 = 10.0;
+let item_1 = 4.5;
+let pack = item_0 - item_1;
+console.log(pack + " subtraction items");
+
+// Boolean
+let Age = 25;
+let Age_2 = 21;
+let chack = Age > Age_2;  //this line >
+console.log(chack + " you Age");
+
+let Age_0 = 25;
+let Age_1 = 21;
+let chack_all = Age_0 < Age_1;  //this line <
+console.log(chack_all + " you Age");
+
+// String
+let F_Name = "vikash ";
+let L_Name = "kushwaha";
+let add = F_Name + L_Name;
+console.log("Your Name: " + add);
+
+// null
+
+let N = null;
+let M = " kushwaha";
+let data = N + M;
+console.log("your data " + data);
+
+//Dynamic Typing
+
+let Number_0 = 20;
+Number_0 = "vikash";
+console.log(Number_0);
+
